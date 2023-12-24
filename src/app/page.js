d05d7ad2445c1f409d043622a21935ea42699b64
jaimin <pkano455@gmail.com>
2023-12-24 11:08:12 +0530

    feed

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,38 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react'
+import Feed from '@/components/Feed'
+import { auth } from '@/utils/firebase'
+import Loader from '@/components/Loader'
 
 export default function Home() {
-  return (
-    <>
-    </>
-  )
+    const [user, setUser] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const router = useRouter()
+
+    useEffect(() => {
+        const unsubscribe = auth.onAuthStateChanged((authUser) => {
+            if (authUser) {
+                setUser(authUser)
+            } else {
+                setUser(null)
+                router.push('/registration/login')
+            }
+            setIsLoading(false)
+        })
+        return () => unsubscribe()
+    }, [router])
+
+    if (isLoading) {
+        return <Loader />
+    }
+
+    return (
+        user && (
+            <section>
+                <Feed authUser={user} />
+            </section>
+        )
+    )
 }
