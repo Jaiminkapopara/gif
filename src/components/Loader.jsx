1ee6b2df0bfbcf9810709343531d865fcfc3dfe5
jaimin <pkano455@gmail.com>
2024-01-04 18:51:40 +0530

    alt added to loading image

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Loader = () => {
   return (
     <div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%]">
-            <Image src='/loader.gif' width={80} height={80}/>
+            <Image src='/loader.gif' width={80} height={80} alt='Loading'/>
         </div>
   )
 }
